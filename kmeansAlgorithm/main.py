6f0d5317e3d914cbdc7b020367648b2b247898bf
Jakub Kowalski <s20802@pjwstk.edu.pl>
2022-04-25 22:28:24 +0200

    centroid initialization commit

--- kmeansAlgorithm/main.py
+++ kmeansAlgorithm/main.py
@@ -1,10 +1,40 @@
+import inline as inline
 import pandas as pd
+import numpy as np
+
+# Reading file into program.
 
 
 def read_csv():
 
-    file = pd.read_csv("Data/iris_cluster_data.txt")
-    print(file)
+    _file = pd.read_csv("Data/iris_cluster_data.txt", header=None)
+
+    return _file
 
 
-read_csv()
+# Initializing random centroids.
+
+
+def initialize_centroids(data, k):
+
+    n_dims = data.shape[1]
+    centroids_min = data.min().min()
+    centroids_max = data.max().max()
+    _centroids = []
+
+    for c in range(k):
+        centroid = np.random.uniform(centroids_min, centroids_max, n_dims)
+        _centroids.append(centroid)
+        # print(centroid)
+
+    _centroids = pd.DataFrame(_centroids, columns=data.columns)
+    print(_centroids)
+    return _centroids
+
+
+file = read_csv()
+centroids = initialize_centroids(file, 3)
+
+
+
+
